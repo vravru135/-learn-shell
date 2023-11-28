e28dab55c9aad40487f4abb84799dbbe6c2a36c6
vravru135 <vravru135@gmail.com>
2023-11-28 15:15:03 +0530

    Making Shell Scripting topics- Add underline

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,7 @@
 dnf install nginx -y
 
+cp expense.conf /etc/nginx/default.d/expense.conf
+
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
@@ -7,4 +9,3 @@ curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zi
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp expense.conf /etc/nginx/default.d/expense.conf
