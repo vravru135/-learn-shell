2a76cddbf87e91d2cd0930020f2360deb7329716
vravru135 <vravru135@gmail.com>
2023-11-28 18:06:31 +0530

    Making Shell Scripting topics- Add underline

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,20 @@
 echo -e "\e[36m Installing Nginx \e[0m"
 dnf install nginx -y
 
+echo -e "\e[36m Expense Config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
 
+echo -e "\e[36m Old Nginx Content \e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[36m Download Frontend Application Code  \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e[36m Extract Downloaded Application Content \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+echo -e "\e[36m Starting Nginx Service \e[0m"
 systemctl enable nginx
 systemctl restart nginx
 
