197fe784dd87af417bfe21806bfea4ac8a9eebe5
vravru135 <vravru135@gmail.com>
2023-12-02 22:03:27 +0530

    Making Shell Scripting topics- Add underline

--- backend.sh
+++ backend.sh
@@ -1,25 +1,57 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+log_file=/tmp/expense.log
+color="\e[33m"
 
-dnf install nodejs -y
+echo -e "${color} disable nodejs default version \e[0m"
+dnf module disable nodejs -y "&>>$log_file"
+echo$ ?
 
-cp backend.service /etc/systemd/system/backend.service
+echo -e "${color} enable nodejs 18 version  \e[0m"
+dnf module enable nodejs:18 -y "&>>$log_file"
+echo$ ?
 
-useradd expense
+echo -e "${color} install nodejs \e[0m"
+dnf install nodejs -y "&>>$log_file"
+echo$ ?
 
-mkdir /app
+echo -e "${color} copy backend service file \e[0m"
+cp backend.service /etc/systemd/system/backend.service "&>>$log_file"
+echo$ ?
 
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
-cd /app
-unzip /tmp/backend.zip
+echo -e "${color} add application user \e[0m"
+useradd expense "&>>$log_file"
+echo$ ?
 
-cd /app
-npm install
+echo -e "${color} create application directory \e[0m"
+mkdir /app "&>>$log_file"
+echo$ ?
 
-dnf install mysql -y
-mysql -h mysql-dev.vravru135.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo -e "${color} delete old application content \e[0m"
+rm -rf /app/* "&>>$log_file"
+echo$ ?
 
+echo -e "${color} download application content \e[0m"
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip "&>>$log_file"
+echo$ ?
 
-systemctl daemon-reload
-systemctl enable backend
-systemctl restart backend
+echo -e "${color} extract application content \e[0m"
+cd /app "&>>$log_file"
+unzip /tmp/backend.zip "&>>$log_file"
+echo$ ?
+
+echo -e "${color} download nodejs dependencies \e[0m"
+npm install "&>>$log_file"
+echo$ ?
+
+echo -e "${color} install mysql client to load schema \e[0m"
+dnf install mysql -y  "&>>$log_file"
+echo$ ?
+
+echo -e "${color} load schema \e[0m"
+mysql -h mysql-dev.vravru135.online -uroot -pExpenseApp@1 < /app/schema/backend.sql "&>>$log_file"
+echo$ ?
+
+echo -e "${color} starting backend service \e[0m"
+systemctl daemon-reload "&>>$log_file"
+systemctl enable backend "&>>$log_file"
+systemctl restart backend "&>>$log_file"
+echo$ ?
