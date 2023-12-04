19343033a175f29606b2d5cd0268472e4b0fc283
vravru135 <vravru135@gmail.com>
2023-12-04 21:16:04 +0530

    Making Shell Scripting topics- Add underline

--- backend.sh
+++ backend.sh
@@ -37,10 +37,11 @@ id expense &>>$log_file
 if [ $? -ne 0 ]; then
 echo -e "${color} Add Application user \e[0m"
 useradd expense &>>$log_file
-if [ $? -eq 0 ]; then
-   echo -e "\e[32m SUCCESS \e[0m"
-else
-   echo -e "\e[31m FAILURE \e[0m"
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+  fi
 fi
 
 echo -e "${color} Create Application Directory \e[0m"
