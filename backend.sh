6bb45f772bd92e8a4653aaab293ebdcdedfc687e
vravru135 <vravru135@gmail.com>
2023-12-05 14:32:24 +0530

    Making Shell Scripting topics- Add underline

--- backend.sh
+++ backend.sh
@@ -1,5 +1,4 @@
-log_file=/tmp/expense.log
-color="\e[33m"
+source common.sh
 
 if [ -z "$1" ]; then
   echo Password input Missing
@@ -7,14 +6,6 @@ if [ -z "$1" ]; then
 fi
 
 MYSQL_ROOT_PASSWORD=$1
-
-status_check() {
-  if [ $? -eq 0 ]; then
-     echo -e "\e[32m SUCCESS \e[0m"
-  else
-     echo -e "\e[31m FAILURE \e[0m"
-  fi
-}
 
 echo -e "${color} Disable Nodejs Default Version \e[0m"
 dnf module disable nodejs -y &>>$log_file
