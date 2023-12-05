bfab6f0e9c5e62ee5ea10eebeabc1847cfc315c9
vravru135 <vravru135@gmail.com>
2023-12-05 14:43:02 +0530

    Making Shell Scripting topics- Add underline

--- frontend.sh
+++ frontend.sh
@@ -1,31 +1,30 @@
-log_file=/tmp/expense.log
-color="\e[33m"
+source common.sh
 
 echo -e "${color} installing nginx \e[0m"
 dnf install nginx -y &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Copy Expense Config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} clean Old Nginx Content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Download Frontend Application Code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Extract Downloaded Application Content \e[0m"
 cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Starting Nginx Service \e[0m"
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
-echo $?
+status_check
 
 
 
