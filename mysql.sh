a28d2ec65c8a0163cabb256637824d1e3a373516
vravru135 <vravru135@gmail.com>
2023-12-05 14:47:59 +0530

    Making Shell Scripting topics- Add underline

--- mysql.sh
+++ mysql.sh
@@ -1,25 +1,24 @@
-log_file=/tmp/expense.log
-color="\e[33m"
+source common.sh
 
-echo -e "${color} disable mysql default version  \e[0m"
+echo -e "${color} Disable mysql default version  \e[0m"
 dnf module disable mysql -y &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} copy mysql repo file \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} install mysql server \e[0m"
 dnf install mysql-community-server -y &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} start mysql server \e[0m"
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} set mysql passward \e[0m"
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
-echo $?
+status_check
 
 
